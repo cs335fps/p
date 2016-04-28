20d97bc0186e519ed6ede177d803e7603aedd6f8
student <student@lab-315-002.cs.csubak.edu>
2016-04-28 10:36:18 -0700

    Stubbed mob, enemy, munitions.

--- charlesE.h
+++ charlesE.h
@@ -1,15 +1,28 @@
 #ifndef _ai_h_
 #define _ai_h_
 
-class mob {
+class mob
+{
+    float hp;
 
-void __mob();
-void spawn(vec location);
-
-
-
+    void __mob();
+    void spawn(vec location);
+    void die();
+    void damage(int health);
+    void move();
 }
 
+class enemy: public mob
+{
 
+    void move;
+}
+class munition: public mob
+{
 
+}
+class npc: public mob
+{
+
+}
 #endif
