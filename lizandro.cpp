dcff2b2fa04173ce080f88c97099cec989e52775
lizandro7268 <lizandro7268@gmail.com>
2016-05-03 21:12:37 -0700

    cleaned up file for hw

--- lizandro.cpp
+++ lizandro.cpp
@@ -1,6 +1,5 @@
-//Finally a simple OpenAL example program.
-//Gordon Griesel
-//2015
+//Lizandro Perez
+//Sound Files
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -8,36 +7,26 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include </usr/include/AL/alut.h>
-
 #include "lizandro.h"
-	ALuint alBuffer;
-	ALuint alSource;
+
+ALuint alBuffer;
+ALuint alSource;
+//initalize and add source of sound
 int initopenal()
 {
-	//Get started right here.
 	alutInit(0, NULL);
 	if (alGetError() != AL_NO_ERROR) {
 		printf("ERROR: alutInit()\n");
 		return 0;
 	}
-	//Clear error state.
 	alGetError();
-	//
-	//Setup the listener.
-	//Forward and up vectors are used.
 	float vec[6] = {0.0f,0.0f,1.0f, 0.0f,1.0f,0.0f};
 	alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f);
 	alListenerfv(AL_ORIENTATION, vec);
 	alListenerf(AL_GAIN, 1.0f);
-	//
-	//Buffer holds the sound information.
 	alBuffer = alutCreateBufferFromFile("./ninemm.wav");
-	//
-	//Source refers to the sound.
-	//Generate a source, and store it in a buffer.
 	alGenSources(1, &alSource);
 	alSourcei(alSource, AL_BUFFER, alBuffer);
-	//Set volume and pitch to normal, no looping of sound.
 	alSourcef(alSource, AL_GAIN, 1.0f);
 	alSourcef(alSource, AL_PITCH, 1.0f);
 	alSourcei(alSource, AL_LOOPING, AL_FALSE);
@@ -45,33 +34,24 @@ int initopenal()
 		printf("ERROR: setting source\n");
 		return 0;
 	}
-return 0;
+	return 0;
 }
+
+//will play sound
 void openal_sound()
 {
-	for (int i=0; i<4; i++) {
 		alSourcePlay(alSource);
-	//	usleep(250000);
-	}
 }
-	
+
+//will clean and delete and sound files
 void clean_al()
 {
-	//Cleanup.
-	//First delete the source.
 	alDeleteSources(1, &alSource);
-	//Delete the buffer.
 	alDeleteBuffers(1, &alBuffer);
-	//Close out OpenAL itself.
-	//Get active context.
 	ALCcontext *Context = alcGetCurrentContext();
-	//Get device for active context.
 	ALCdevice *Device = alcGetContextsDevice(Context);
-	//Disable context.
 	alcMakeContextCurrent(NULL);
-	//Release context(s).
 	alcDestroyContext(Context);
-	//Close device.
 	alcCloseDevice(Device);
 }
 
