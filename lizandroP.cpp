5d2716a9858bd5d90c35a5c322a09bfd3fd7e8a9
lizandro7268 <lizandro7268@gmail.com>
2016-05-16 19:51:45 +0000

    update my file

--- lizandroP.cpp
+++ lizandroP.cpp
@@ -1,5 +1,8 @@
 //Lizandro Perez
+//cs335 software engineering 
+//Prof. Gordon
 //Sound Files
+//
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -10,9 +13,14 @@
 #include </usr/include/AL/alut.h>
 #include "lizandroP.h"
 
+//sounds buffers
+ALuint alBuffer0;
+ALuint alBuffer1;
+ALuint alBuffer2;
 
-ALuint alBuffer;
-ALuint alSource;
+//sound source 
+ALuint alSource[3];
+
 //initalize and add source of sound
 int initopenal()
 {
@@ -21,17 +29,40 @@ int initopenal()
 		printf("ERROR: alutInit()\n");
 		return 0;
 	}
+
 	alGetError();
+
 	float vec[6] = {0.0f,0.0f,1.0f, 0.0f,1.0f,0.0f};
 	alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f);
 	alListenerfv(AL_ORIENTATION, vec);
 	alListenerf(AL_GAIN, 1.0f);
-	alBuffer = alutCreateBufferFromFile("./ninemm.wav");
-	alGenSources(1, &alSource);
-	alSourcei(alSource, AL_BUFFER, alBuffer);
-	alSourcef(alSource, AL_GAIN, 1.0f);
-	alSourcef(alSource, AL_PITCH, 1.0f);
-	alSourcei(alSource, AL_LOOPING, AL_FALSE);
+
+	//load sound file into buffer 
+	alBuffer0 = alutCreateBufferFromFile("./shot.wav");
+	alBuffer1 = alutCreateBufferFromFile("./shot.wav");
+	alBuffer2 = alutCreateBufferFromFile("./ninemm.wav");
+	alGenSources(1, &alSource[0]);
+	alGenSources(1, &alSource[1]);
+	alGenSources(1, &alSource[2]);
+	
+	//setting for first sound 
+	alSourcei(alSource[0], AL_BUFFER, alBuffer0);
+	alSourcef(alSource[0], AL_GAIN, 1.0f);
+	alSourcef(alSource[0], AL_PITCH, 1.0f);
+	alSourcei(alSource[0], AL_LOOPING, AL_FALSE);
+	
+	//setting for second sound 
+	alSourcei(alSource[1], AL_BUFFER, alBuffer1);
+	alSourcef(alSource[1], AL_GAIN, 2.0f);
+	alSourcef(alSource[1], AL_PITCH, 1.0f);
+	alSourcei(alSource[1], AL_LOOPING, AL_FALSE);
+
+	//setting for third sound
+	alSourcei(alSource[2], AL_BUFFER, alBuffer2);
+	alSourcef(alSource[2], AL_GAIN, 1.0f);
+	alSourcef(alSource[2], AL_PITCH, 1.0f);
+	alSourcei(alSource[2], AL_LOOPING, AL_FALSE);
+
 	if (alGetError() != AL_NO_ERROR) {
 		printf("ERROR: setting source\n");
 		return 0;
@@ -39,17 +70,26 @@ int initopenal()
 	return 0;
 }
 
-//will play sound
+//function to play sound
 void openal_sound()
 {
-		alSourcePlay(alSource);
+	//play selected sound
+	alSourcePlay(alSource[2]);
 }
 
-//will clean and delete and sound files
+//Function to clean and delete sound files
 void clean_al()
 {
-	alDeleteSources(1, &alSource);
-	alDeleteBuffers(1, &alBuffer);
+	//clear source 
+	alDeleteSources(1, &alSource[0]);
+	alDeleteSources(1, &alSource[1]);
+	alDeleteSources(1, &alSource[2]);
+
+	//clear buffer
+	alDeleteBuffers(1, &alBuffer0);
+	alDeleteBuffers(1, &alBuffer1);
+	alDeleteBuffers(1, &alBuffer2);
+
 	ALCcontext *Context = alcGetCurrentContext();
 	ALCdevice *Device = alcGetContextsDevice(Context);
 	alcMakeContextCurrent(NULL);
